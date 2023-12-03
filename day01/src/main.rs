63b233109fb0bd614d5136be92f8a4008f7731e7
Nina <nina.k.jensen@gmail.com>
2023-12-03 09:26:51 +0000

    Refactor day 1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,33 +1,20 @@
 use aho_corasick::AhoCorasick;
 use std::include_str;
 
-fn day01a(input_text: &'static str) -> i32 {
+/// Returns the sum of the concatenated value of the first and last number found in `input_text`
+///
+/// Uses Aho-Corasick to find overlapping matches, and the pattern ID mod 9 + 1 to get the integer value
+/// of the match. A bit hacky, and requires the input vec to be ordered i.e. `["1", ..., "9", "one", ..., "nine"]`
+/// # Arguments
+/// * `input_text` - A string from include_str
+/// * `pattern` - The Aho-Corasick pattern to match for.
+fn sum_first_last(input_text: &'static str, pattern: &Vec<&str>) -> i32 {
+    let ac = AhoCorasick::new(pattern).unwrap();
     input_text
         .lines()
-        .map(|l| {
-            l.chars()
-                .filter_map(|c: char| c.to_digit(10))
-                .collect::<Vec<_>>()
-        })
-        .map(|list| {
-            format!("{}{}", list.first().unwrap(), list.last().unwrap())
-                .parse::<i32>()
-                .unwrap()
-        })
-        .sum()
-}
-
-fn day01b(input_text: &'static str) -> i32 {
-    let patterns = [
-        "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six",
-        "seven", "eight", "nine",
-    ];
-    let ac = AhoCorasick::new(patterns).unwrap();
-    input_text
-        .lines()
-        .map(|l| {
-            ac.find_overlapping_iter(l)
-                .map(|mat| mat.pattern().as_usize() % 9 + 1)
+        .map(|line| {
+            ac.find_overlapping_iter(line)
+                .map(|mat| mat.pattern().as_usize() % 9 + 1) // mod 9 needed for part b
                 .collect::<Vec<_>>()
         })
         .map(|list| {
@@ -40,8 +27,12 @@ fn day01b(input_text: &'static str) -> i32 {
 
 fn main() {
     let input_text = include_str!("./day01");
-    println!("{:?}", day01a(input_text));
-    println!("{:?}", day01b(input_text))
+    let mut pattern = vec!["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    println!("part a: {:?}", sum_first_last(input_text, &pattern));
+    pattern.append(&mut vec![
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ]);
+    println!("part b: {:?}", sum_first_last(input_text, &pattern))
 }
 
 #[cfg(test)]
@@ -51,11 +42,16 @@ mod tests {
     #[test]
     fn test_a() {
         let a = include_str!("day01a_test");
-        assert_eq!(142, day01b(a));
+        let a_pat = vec!["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+        assert_eq!(142, sum_first_last(a, &a_pat));
     }
     #[test]
     fn test_b() {
         let b = include_str!("day01b_test");
-        assert_eq!(281, day01b(b))
+        let b_pat = vec![
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five",
+            "six", "seven", "eight", "nine",
+        ];
+        assert_eq!(281, sum_first_last(b, &b_pat))
     }
 }
